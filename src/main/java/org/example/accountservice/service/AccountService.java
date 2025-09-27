694c1b5a26d86ea1801624041b153de8d8ea01f3
alyvnihad <aliyevnihad011@gmail.com>
2025-09-27 18:26:33 +0400

    Account register commit - Account Service

---
+++ src/main/java/org/example/accountservice/service/AccountService.java
@@ -0,0 +1,35 @@
+package org.example.accountservice.service;
+
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.example.accountservice.model.Account;
+import org.example.accountservice.model.Currency;
+import org.example.accountservice.repository.AccountRepository;
+import org.springframework.stereotype.Service;
+
+
+import java.time.LocalDate;
+
+@Service
+@RequiredArgsConstructor
+public class AccountService {
+
+    private final AccountRepository accountRepository;
+
+    public void register(Long cardNumber, Currency currency) {
+        Long byAccountNumber = generateUUID();
+        Account account = new Account();
+        account.setAccountNumber(byAccountNumber);
+        account.setCardNumber(cardNumber);
+        account.setBalance(0);
+        account.setCurrency(currency);
+        account.setTransferLimit(2000);
+        account.setTransferLimitTime(LocalDate.now());
+        accountRepository.save(account);
+    }
+
+    private Long generateUUID() {
+        return System.currentTimeMillis() / 100;
+    }
+
+}
