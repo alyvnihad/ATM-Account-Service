cad5c02acc2f73f10871956acfa986d2e71e2e0f
alyvnihad <aliyevnihad011@gmail.com>
2025-09-27 18:40:15 +0400

    update commit - AccountService

--- src/main/java/org/example/accountservice/AccountServiceApplication.java
+++ src/main/java/org/example/accountservice/AccountServiceApplication.java
@@ -4,7 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.time.LocalDate;
+
 
 @SpringBootApplication
 @Slf4j
@@ -12,7 +12,6 @@ public class AccountServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AccountServiceApplication.class, args);
-//        log.info(String.valueOf(LocalDate.of(2025,9,27).isBefore(LocalDate.now())));
     }
 
 }
