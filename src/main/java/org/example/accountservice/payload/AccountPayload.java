a9b94264c24abde88fde9eb19c21efb1b10ca4a2
alyvnihad <aliyevnihad011@gmail.com>
2025-10-09 13:51:36 +0400

    Payload add commit - Account

---
+++ src/main/java/org/example/accountservice/payload/AccountPayload.java
@@ -0,0 +1,15 @@
+package org.example.accountservice.payload;
+
+import lombok.Data;
+
+@Data
+public class AccountPayload {
+    private Long accountId;
+    private Long accountNumber;
+    private Long cardNumber;
+    private Double amount;
+    private Long atmId;
+    private double balance;
+    private String currency;
+    private double transferLimit;
+}
