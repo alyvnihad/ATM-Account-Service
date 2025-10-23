0478c1adf359555eb3516ae00db8f09b59cd0153
alyvnihad <aliyevnihad011@gmail.com>
2025-10-23 11:45:04 +0400

    AccountService service class update.v1 commit - Account Service

--- src/main/java/org/example/accountservice/service/AccountService.java
+++ src/main/java/org/example/accountservice/service/AccountService.java
@@ -2,6 +2,7 @@ package org.example.accountservice.service;
 
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.example.accountservice.dto.AccountResponse;
 import org.example.accountservice.dto.TransactionRequest;
 import org.example.accountservice.model.Account;
 import org.example.accountservice.model.Currency;
@@ -14,6 +15,7 @@ import org.springframework.web.client.RestTemplate;
 
 import java.time.LocalDate;
 
+// Account service creates a new registry account, account transactions, and account identification number
 @Service
 @RequiredArgsConstructor
 public class AccountService {
@@ -24,18 +26,28 @@ public class AccountService {
     @Value("${transaction.url}")
     private String transactionUrl;
 
-    public void register(AccountPayload payload) {
+    // Register a new account
+    public AccountResponse register(AccountPayload payload) {
+
+        // Account identification number generate
         Long byAccountNumber = generateUUID();
+
+        // Create a new account
         Account account = new Account();
         account.setAccountNumber(byAccountNumber);
         account.setCardNumber(payload.getCardNumber());
+        account.setEmail(payload.getEmail());
         account.setBalance(0);
         account.setCurrency(Currency.valueOf(payload.getCurrency()));
         account.setTransferLimit(2000);
         account.setTransferLimitTime(LocalDate.now().withDayOfMonth(LocalDate.now().lengthOfMonth()));
         accountRepository.save(account);
+        AccountResponse accountResponse = new AccountResponse();
+        accountResponse.setAccountNumber(account.getAccountNumber());
+        return accountResponse;
     }
 
+    // Deposit money and send log to Transaction service
     @Transactional
     public void deposit(AccountPayload payload) {
         Account account = accountRepository.findByCardNumber(payload.getCardNumber())
@@ -44,12 +56,26 @@ public class AccountService {
             if (payload.getAmount() > 0) {
                 account.setBalance(account.getBalance() + payload.getAmount());
                 accountRepository.save(account);
-            }else{
+            } else {
                 throw new RuntimeException("Your deposit greater than zero. Amount: " + payload.getAmount());
             }
+
+            // Prepare transaction request
+            TransactionRequest request = new TransactionRequest();
+            request.setAccountId(account.getId());
+            request.setCardNumber(account.getCardNumber());
+            request.setEmail(account.getEmail());
+            request.setType("deposit");
+            request.setAmount(payload.getAmount());
+            request.setAtmId(payload.getAtmId());
+
+            // Transaction service call send log
+            restTemplate.postForEntity(transactionUrl + "/log", request, TransactionRequest.class);
         }
+
     }
 
+    // Withdraw money and send log to Transaction service
     @Transactional
     public void withdraw(AccountPayload payload) {
         Account account = accountRepository.findByCardNumber(payload.getCardNumber())
@@ -68,22 +94,27 @@ public class AccountService {
                 throw new RuntimeException("Transfer limit: " + account.getTransferLimit());
             }
 
+            // Prepare transaction request
             TransactionRequest request = new TransactionRequest();
             request.setAccountId(account.getId());
             request.setCardNumber(account.getCardNumber());
+            request.setEmail(account.getEmail());
             request.setType("withdraw");
             request.setAmount(payload.getAmount());
             request.setAtmId(payload.getAtmId());
+
+            // Transaction service call send log
             restTemplate.postForEntity(transactionUrl + "/log", request, TransactionRequest.class);
         }
     }
 
+    // Get current account balance
     public double getBalance(AccountPayload payload) {
         Account account = accountRepository.findByCardNumber(payload.getCardNumber()).orElseThrow(() -> new RuntimeException("error"));
         return account.getBalance();
     }
 
-
+    // Generate unique account number
     private Long generateUUID() {
         return System.currentTimeMillis() / 100;
     }
